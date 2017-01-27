044ea59dce6c567e1d5b7a90fb1db60f6aad5ab2
aadriance <aadriance@icloud.com>
2017-01-26 22:43:29 -0800

    Initial game state manager

---
+++ ProjM1/src/GameState.cpp
@@ -0,0 +1,10 @@
+//
+//  GameState.cpp
+//  ProjM1
+//
+//  Created by Andrew Adriance on 1/26/17.
+//
+//
+
+#include <stdio.h>
+#include "GameState.h"
---
+++ ProjM1/src/GameState.h
@@ -0,0 +1,25 @@
+//
+//  GameState.h
+//  ProjM1
+//
+//  Created by Andrew Adriance on 1/26/17.
+//
+//
+
+#ifndef GameState_h
+#define GameState_h
+
+#include <GLFW/glfw3.h>
+
+class GameState {
+public:
+    float worldTime = 0; //Current time in the world total
+    float deltaTime = 0; //Time since last update
+    int waveNumber = 0;  //How many waves you've survived
+    float mouseX = 0;    //Current position of mouse (X)
+    float mouseY = 0;    //Current position of mouse (Y)
+    GLFWwindow &window;
+    //use glfwGetKey (GLFWwindow *window, int key) to check key input
+};
+
+#endif /* GameState_h */
