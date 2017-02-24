fb1bae2692f569faef8659f9f715fe988e65bed6
aadriance <aadriance@icloud.com>
2017-02-23 22:06:09 -0800

    sheeo body

--- ProjM1/src/main.cpp
+++ ProjM1/src/main.cpp
@@ -123,7 +123,7 @@ static void init()
 
 	world.makeFence(12, 22);
 
-    EnemyGameObj *enemy = new EnemyGameObj(getShape("sheepLeg"), getTexture("legText"));
+    EnemyGameObj *enemy = new EnemyGameObj(getShape("sheepBod"), getTexture("sheepBodText"));
     enemy->setPos(42, 2, 45);
 
     world.addObj(enemy);
