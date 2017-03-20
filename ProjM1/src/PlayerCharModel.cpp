f1518f2a9e218f8552e73a02d96ccd31629924cf
Jeffrey Lam <jeffrey.clam95@gmail.com>
2017-03-20 00:05:49 -0700

    Definitely looks like an old man now

--- ProjM1/src/PlayerCharModel.cpp
+++ ProjM1/src/PlayerCharModel.cpp
@@ -16,31 +16,35 @@ void PlayerCharModel::init_Model() {
         body[TORSO]->offset = vec3(0, 0, 0);
 
         body[HEAD]->offset = vec3(0, .50, 0);
-        body[HEAD]->attach_Limb = vec3(0, .65, 0);
+        body[HEAD]->attach_Limb = vec3(0, .5, .5);
 
+        body[LEFT_ARM]->scale = vec3(.75, .75, .75);
         body[LEFT_ARM]->offset = vec3(0, -1.0, 0);
         body[LEFT_ARM]->rotation = vec3(0, 0, 1);
         body[LEFT_ARM]->action_rotation = vec3(1, 0, 0);
-        body[LEFT_ARM]->attach_Limb = vec3(-.9, .30, 0);
+        body[LEFT_ARM]->attach_Limb = vec3(-.8, .50, .40);
 
-        body[CANE]->scale = vec3(.25, .5, .25);
+        body[CANE]->scale = vec3(.25, .80, .25);
         body[CANE]->offset = vec3(0, -2.5, 0);
         body[CANE]->rotation = vec3(0, 0, 1);
         body[CANE]->action_rotation = vec3(1, 0, 0);
-        body[CANE]->attach_Limb = vec3(-.9, .3, 0);
+        body[CANE]->attach_Limb = vec3(-.8, .50, .40);
 
+        body[RIGHT_ARM]->scale = vec3(.75, .75, .75);
         body[RIGHT_ARM]->offset = vec3(0, -1.0, 0);
         body[RIGHT_ARM]->rotation = vec3(0, 0, 1);
         body[RIGHT_ARM]->action_rotation = vec3(1, 0, 0);
-        body[RIGHT_ARM]->attach_Limb = vec3(.9, .30, 0);
+        body[RIGHT_ARM]->attach_Limb = vec3(.8, .50, .40);
 
-        body[LEFT_LEG]->offset = vec3(0, -.35, 0);
+        body[LEFT_LEG]->scale = vec3(.75, .65, .75);
+        body[LEFT_LEG]->offset = vec3(0, -1, 0);
         body[LEFT_LEG]->action_rotation = vec3(1, 0, 0);
-        body[LEFT_LEG]->attach_Limb = vec3(-.50, -1.5, 0);
+        body[LEFT_LEG]->attach_Limb = vec3(-.50, -.75, 0.25);
 
-        body[RIGHT_LEG]->offset = vec3(0, -.35, 0);
+        body[RIGHT_LEG]->scale = vec3(.75, .65, .75);
+        body[RIGHT_LEG]->offset = vec3(0, -1, 0);
         body[RIGHT_LEG]->action_rotation = vec3(1, 0, 0);
-        body[RIGHT_LEG]->attach_Limb = vec3(.50, -1.5, 0);
+        body[RIGHT_LEG]->attach_Limb = vec3(.50, -.75, 0.25);
     }
 }
 
@@ -65,12 +69,12 @@ bool PlayerCharModel::scare_Motion() {
 void PlayerCharModel::walk_Motion() {
     if (walk) {
         legs -= 0.05;
-        if (legs < -1)
+        if (legs < -.5)
             walk = 0;
     }
     else {
         legs += 0.05;
-        if (legs > 1) {
+        if (legs > .5) {
             walk = 1;
         }
     }
